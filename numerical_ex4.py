54f0b294b6aa3b24712e0e36ef46bf4b91dede39
Manan Mittal <58558528+me2190901@users.noreply.github.com>
2022-08-27 17:45:45 +0530

    added file for numerical example 4 and done initiation part

---
+++ numerical_ex4.py
@@ -0,0 +1,120 @@
+import numpy as np
+import math
+import time
+from itertools import product
+
+np.random.seed(1234)
+
+# ---------------------Initialization-----------------
+n = 6
+
+def define_R():
+    R = [1/64 for i in range(64)]
+    return R
+
+def define_N_Candidates():
+    Neighbours = {}
+    for i in range(n):
+        for j in range(n):
+            Neighbours[(i,j)]=[((i-1)%n,j%n),((i+1)%n,j%n),(i%n,(j-1)%n),(i%n,(j+1)%n)]
+    return Neighbours
+
+def N(x):
+    return list(product(N_Candidates[x[0]],N_Candidates[x[1]],N_Candidates[x[2]]))
+
+def Mk(k):
+    return math.floor(math.log(k+10, 5))
+
+# def f(x):
+#     f_vals = [0.3, 0.7, 0.9, 0.5, 1, 1.4, 0.7, 0.8, 0.7, 0.6, 2.1, 2.2, 1.8, 0.1, 0.3, 0.7, 0.9, 1.1, 1.2, 1.4, 1.9, 0.2, 2.1, 2.2, 2.1, 2, 1.9, 1.2, 1.1, 0.7, 0.3, 0.1, 0.1, 1.5, 1.3, 1.2, 1.2, 0.3, 1.6, 0.8, 0.6, 0.7, 1.3, 1.9, 2, 0, 0.8, 0.3, 0.5, 0.6, 1.3, 1.5, 1.5, 1.8, 0.8, 2, 1.9, 1.1, 0.2, 1.3, 0.6, 0.3, 0.5, 0.6, 0.7, 0.9, 1.3, 1.4, 1.6, 0.2, 0.1, 0.8, 0.7, 0.8, 0.9, 0.7, 0.6, 0.5, 0.9, 0.1, 1.4, 1.6, 0.3, 1.1, 0.6, 0.9, 0.1, 2.1, 0.9, 1.8, 1.7, 1.7, 1.3, 1.2, 0.4, 0.9, 1.3, 1.1, 1.9, 1.4]
+#     return f_vals[x-1]
+
+def find_x0():
+    x0 = list(map(tuple,np.random.randint(0, n,[3,2])))
+    return x0
+
+# def get_h_val(f_x):
+#     temp = np.random.uniform(low=f_x - 0.5, high=f_x + 0.5, size=1)  # [low,high)
+#     return temp[0]
+
+def get_theta_val(a, b):
+    temp = np.random.uniform(low=a, high=b, size=1)  # [low,high)
+    return temp[0]
+
+# Defining parameters for Theta
+a = 250
+b = 800
+
+# Defining Neighbourhood Structure(N)
+N_Candidates = define_N_Candidates()
+
+# Defining Transition Probability Matrix(R)
+R = define_R()
+
+# Number of iterations whole algo is to be run
+iter_count = 1
+
+# # Optimal f(x) as per the paper
+# optimal_f = 0
+
+# ---------------Implementing Stochastic Ruler Algorithm------------
+
+
+def step_1(x):
+    Nx = N(x)
+    P = R
+    Represent_Nx=[i for i in range(len(Nx))]
+    i = np.random.choice(Represent_Nx, p=R)
+    return Nx[i]
+
+def step_3():
+    global k
+    k = k + 1
+
+# def step_2(k, xk, z, a, b):
+#     total_tests_to_do = Mk(k)
+#     test_count = 0
+#     while (test_count < total_tests_to_do):
+#         # realization for h(z) i.e. uniform random between f(z)-0.5 , f(z)+0.5
+#         h_z = get_h_val(f(z))
+#         theta_val = get_theta_val(a, b)
+#         if (h_z > theta_val):
+#             return 0  # go to next step i.e. step-3
+#         else:
+#             pass
+#         test_count += 1
+#     return 1
+
+# def stocastic_ruler():
+#     # Defining X0
+#     xk = find_x0()
+#     z = None
+#     while (True):
+#         z = step_1(xk)
+#         check = step_2(k, xk, z, a, b)
+#         if (check == 0):
+#             step_3()
+#             xk = xk
+#         else:
+#             step_3()
+#             xk = z
+#         if (f(xk)==optimal_f):
+#             depth_went.append(k)
+#             break
+
+# depth_went = []
+
+# total_time=0
+# for i in range(iter_count):
+#     print("Iteration", i+1)
+#     k=0
+#     start = time.perf_counter_ns()
+#     stocastic_ruler()
+#     end = time.perf_counter_ns()
+#     total_time+=end-start
+#     # go to step-1
+
+# print("Total Time = ", (total_time)/(10**9), " seconds")
+# print("Average_Time per iteration = ",
+#       (total_time)/(iter_count*(10**9)), "seconds")
+# print("Average k = ", sum(depth_went)/iter_count)
